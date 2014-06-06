3520b82feef3fe5cdf79cd6a8889cccc590add26
Ben McClelland <benjamin.m.mcclelland@intel.com>
2014-06-06 10:02:46 -0700

    add copyright to tests

--- orcm/test/orcm_alloc.c
+++ orcm/test/orcm_alloc.c
@@ -1,7 +1,10 @@
-/* -*- C -*-
+/*
+ * Copyright (c) 2014      Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
  *
  * $HEADER$
- *
  */
 
 #include <stdio.h>

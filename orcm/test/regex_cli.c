324db4b2c8a9f0fc00be7317f59593ec996d1ed1
Ben McClelland <benjamin.m.mcclelland@intel.com>
2014-06-06 10:02:46 -0700

    add copyright to tests

--- orcm/test/regex_cli.c
+++ orcm/test/regex_cli.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2014      Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
 #include "orcm_config.h"
 
 #include <stdio.h>

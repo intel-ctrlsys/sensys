78f99cfb38eca387c637486ba5a2301e5f042249
lrrajesh <l.r.rajeshnarayanan@intel.com>
2014-06-10 17:14:51 -0700

    Adding sst_ojobd component under sst framework

---
+++ orcm/mca/sst/ojobd/sst_ojobd.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014      Intel, Inc.  All rights reserved. 
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#ifndef SST_OJOBD_H
+#define SST_OJOBD_H
+
+#include "orcm/mca/sst/sst.h"
+
+ORCM_DECLSPEC extern orcm_sst_base_component_t mca_orcm_sst_ojobd_component;
+ORCM_DECLSPEC extern orcm_sst_base_module_t orcm_sst_ojobd_module;
+
+#endif /* SST_OJOBD_H */

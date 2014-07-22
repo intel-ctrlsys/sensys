8fd8c4823cde6e2d4cd27b5378ec64b6eb02073f
lrrajesh <l.r.rajeshnarayanan@intel.com>
2014-07-22 09:10:25 -0700

    Help command for orun and cli cleanup

--- orcm/tools/orun/orun.c
+++ orcm/tools/orun/orun.c
@@ -135,295 +135,22 @@ static opal_cmd_line_init_t cmd_line_init[] = {
     { NULL, 'V', NULL, "version", 0,
       &orun_globals.version, OPAL_CMD_LINE_TYPE_BOOL,
       "Print version and exit" },
-    { NULL, 'v', NULL, "verbose", 0,
-      &orun_globals.verbose, OPAL_CMD_LINE_TYPE_BOOL,
-      "Be verbose" },
-    { "orte_execute_quiet", 'q', NULL, "quiet", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Suppress helpful messages" },
-    { NULL, '\0', "report-pid", "report-pid", 1,
-      &orun_globals.report_pid, OPAL_CMD_LINE_TYPE_STRING,
-      "Printout pid on stdout [-], stderr [+], or a file [anything else]" },
-    { NULL, '\0', "report-uri", "report-uri", 1,
-      &orun_globals.report_uri, OPAL_CMD_LINE_TYPE_STRING,
-      "Printout URI on stdout [-], stderr [+], or a file [anything else]" },
-    
-    /* exit status reporting */
-    { "orte_report_child_jobs_separately", '\0', "report-child-jobs-separately", "report-child-jobs-separately", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Return the exit status of the primary job only" },
-    
-    /* hetero apps */
-    { "orte_hetero_apps", '\0', NULL, "hetero-apps", 0,
-        NULL, OPAL_CMD_LINE_TYPE_BOOL,
-    "Indicates that multiple app_contexts are being provided that are a mix of 32/64 bit binaries" },
-    
-    /* select XML output */
-    { "orte_xml_output", '\0', "xml", "xml", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Provide all output in XML format" },
-    { "orte_xml_file", '\0', "xml-file", "xml-file", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Provide all output in XML format to the specified file" },
 
-    /* tag output */
-    { "orte_tag_output", '\0', "tag-output", "tag-output", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Tag all output with [job,rank]" },
-    { "orte_timestamp_output", '\0', "timestamp-output", "timestamp-output", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Timestamp all application process output" },
-    { "orte_output_filename", '\0', "output-filename", "output-filename", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Redirect output from application processes into filename.rank" },
-    { "orte_xterm", '\0', "xterm", "xterm", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Create a new xterm window and display output from the specified ranks there" },
-
-    /* select stdin option */
-    { NULL, '\0', "stdin", "stdin", 1,
-      &orun_globals.stdin_target, OPAL_CMD_LINE_TYPE_STRING,
-      "Specify procs to receive stdin [rank, all, none] (default: 0, indicating rank 0)" },
-    
-    /* request that argv[0] be indexed */
-    { NULL, '\0', "index-argv-by-rank", "index-argv-by-rank", 0,
-      &orun_globals.index_argv, OPAL_CMD_LINE_TYPE_BOOL,
-      "Uniquely index argv[0] for each process using its rank" },
-
-    /* Specify the launch agent to be used */
-    { "orte_launch_agent", '\0', "launch-agent", "launch-agent", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Command used to start processes on remote nodes (default: orted)" },
-    
-    /* Preload the binary on the remote machine */
-    { NULL, 's', NULL, "preload-binary", 0,
-      &orun_globals.preload_binaries, OPAL_CMD_LINE_TYPE_BOOL,
-      "Preload the binary on the remote machine before starting the remote process." },
-
-    /* Preload files on the remote machine */
-    { NULL, '\0', NULL, "preload-files", 1,
-      &orun_globals.preload_files, OPAL_CMD_LINE_TYPE_STRING,
-      "Preload the comma separated list of files to the remote machines current working directory before starting the remote process." },
-
-    /* Use an appfile */
-    { NULL, '\0', NULL, "app", 1,
-      &orun_globals.appfile, OPAL_CMD_LINE_TYPE_STRING,
-      "Provide an appfile; ignore all other command line options" },
-
-    /* Number of processes; -c, -n, --n, -np, and --np are all
+    /* Number of processes; -np, and --np are all
        synonyms */
-    { NULL, 'c', "np", "np", 1,
-      &orun_globals.num_procs, OPAL_CMD_LINE_TYPE_INT,
-      "Number of processes to run" },
-    { NULL, '\0', "n", "n", 1,
+    { NULL, '\0', "np", "np", 1,
       &orun_globals.num_procs, OPAL_CMD_LINE_TYPE_INT,
       "Number of processes to run" },
     
-    /* maximum size of VM - typically used to subdivide an allocation */
-    { "orte_max_vm_size", '\0', "max-vm-size", "max-vm-size", 1,
-      NULL, OPAL_CMD_LINE_TYPE_INT,
-      "Number of processes to run" },
-
-    /* Set a hostfile */
-    { NULL, '\0', "hostfile", "hostfile", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Provide a hostfile" },
-    { NULL, '\0', "machinefile", "machinefile", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Provide a hostfile" },
-    { "orte_default_hostfile", '\0', "default-hostfile", "default-hostfile", 1,
-        NULL, OPAL_CMD_LINE_TYPE_STRING,
-    "Provide a default hostfile" },
-    { "opal_if_do_not_resolve", '\0', "do-not-resolve", "do-not-resolve", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Do not attempt to resolve interfaces" },
-    
-    /* uri of Open MPI server, or at least where to get it */
-    { NULL, '\0', "ompi-server", "ompi-server", 1,
-      &orun_globals.ompi_server, OPAL_CMD_LINE_TYPE_STRING,
-      "Specify the URI of the Open MPI server, or the name of the file (specified as file:filename) that contains that info" },
-    { NULL, '\0', "wait-for-server", "wait-for-server", 0,
-      &orun_globals.wait_for_server, OPAL_CMD_LINE_TYPE_BOOL,
-      "If ompi-server is not already running, wait until it is detected (default: false)" },
-    { NULL, '\0', "server-wait-time", "server-wait-time", 1,
-      &orun_globals.server_wait_timeout, OPAL_CMD_LINE_TYPE_INT,
-      "Time in seconds to wait for ompi-server (default: 10 sec)" },
-    
-    { "carto_file_path", '\0', "cf", "cartofile", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Provide a cartography file" },
-
-    { "orte_rankfile", '\0', "rf", "rankfile", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Provide a rankfile file" },
-
-    /* Export environment variables; potentially used multiple times,
-       so it does not make sense to set into a variable */
-    { NULL, 'x', NULL, NULL, 1,
-      NULL, OPAL_CMD_LINE_TYPE_NULL,
-      "Export an environment variable, optionally specifying a value (e.g., \"-x foo\" exports the environment variable foo and takes its value from the current environment; \"-x foo=bar\" exports the environment variable name foo and sets its value to \"bar\" in the started processes)" },
-
-      /* Mapping controls */
-    { "rmaps_base_display_map", '\0', "display-map", "display-map", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Display the process map just before launch"},
-    { "rmaps_base_display_devel_map", '\0', "display-devel-map", "display-devel-map", 0,
-       NULL, OPAL_CMD_LINE_TYPE_BOOL,
-       "Display a detailed process map (mostly intended for developers) just before launch"},
-    { "rmaps_base_display_topo_with_map", '\0', "display-topo", "display-topo", 0,
-       NULL, OPAL_CMD_LINE_TYPE_BOOL,
-       "Display the topology as part of the process map (mostly intended for developers) just before launch"},
-    { "rmaps_base_display_diffable_map", '\0', "display-diffable-map", "display-diffable-map", 0,
-       NULL, OPAL_CMD_LINE_TYPE_BOOL,
-       "Display a diffable process map (mostly intended for developers) just before launch"},
-    { NULL, 'H', "host", "host", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "List of hosts to invoke processes on" },
-    { "rmaps_base_no_schedule_local", '\0', "nolocal", "nolocal", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Do not run any MPI applications on the local node" },
-    { "rmaps_base_no_oversubscribe", '\0', "nooversubscribe", "nooversubscribe", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Nodes are not to be oversubscribed, even if the system supports such operation"},
-    { "rmaps_base_oversubscribe", '\0', "oversubscribe", "oversubscribe", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Nodes are allowed to be oversubscribed, even on a managed system"},
-    { "rmaps_base_cpus_per_rank", '\0', "cpus-per-proc", "cpus-per-proc", 1,
-      NULL, OPAL_CMD_LINE_TYPE_INT,
-      "Number of cpus to use for each process [default=1]" },
-    { "rmaps_base_cpus_per_rank", '\0', "cpus-per-rank", "cpus-per-rank", 1,
-      NULL, OPAL_CMD_LINE_TYPE_INT,
-      "Synonym for cpus-per-proc" },
-
-    /* backward compatiblity */
-    { "rmaps_base_bycore", '\0', "bycore", "bycore", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Whether to map and rank processes round-robin by core" },
-    { "rmaps_base_bynode", '\0', "bynode", "bynode", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Whether to map and rank processes round-robin by node" },
-    { "rmaps_base_byslot", '\0', "byslot", "byslot", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Whether to map and rank processes round-robin by slot" },
-
-    /* Nperxxx options that do not require topology and are always
-     * available - included for backwards compatibility
-     */
-    { "rmaps_ppr_pernode", '\0', "pernode", "pernode", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Launch one process per available node" },
-    { "rmaps_ppr_n_pernode", '\0', "npernode", "npernode", 1,
-        NULL, OPAL_CMD_LINE_TYPE_INT,
-        "Launch n processes per node on all allocated nodes" },
     { "rmaps_ppr_n_pernode", '\0', "N", NULL, 1,
         NULL, OPAL_CMD_LINE_TYPE_INT,
         "Launch n processes per node on all allocated nodes (synonym for npernode)" },
-
-#if OPAL_HAVE_HWLOC
-    /* declare hardware threads as independent cpus */
-    { "hwloc_base_use_hwthreads_as_cpus", '\0', "use-hwthread-cpus", "use-hwthread-cpus", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Use hardware threads as independent cpus" },
-
-    /* include npersocket for backwards compatibility */
-    { "rmaps_ppr_n_persocket", '\0', "npersocket", "npersocket", 1,
-      NULL, OPAL_CMD_LINE_TYPE_INT,
-      "Launch n processes per socket on all allocated nodes" },
-
-    /* Mapping options */
-    { "rmaps_base_mapping_policy", '\0', NULL, "map-by", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Mapping Policy [slot | hwthread | core | socket (default) | numa | board | node]" },
-
-      /* Ranking options */
-    { "rmaps_base_ranking_policy", '\0', NULL, "rank-by", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Ranking Policy [slot (default) | hwthread | core | socket | numa | board | node]" },
-
-      /* Binding options */
-    { "hwloc_base_binding_policy", '\0', NULL, "bind-to", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Policy for binding processes [none | hwthread | core (default) | socket | numa | board] (supported qualifiers: overload-allowed,if-supported)" },
-
-    /* backward compatiblity */
-    { "hwloc_base_bind_to_core", '\0', "bind-to-core", "bind-to-core", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Bind processes to cores" },
-    { "hwloc_base_bind_to_socket", '\0', "bind-to-socket", "bind-to-socket", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Bind processes to sockets" },
-
-    { "hwloc_base_report_bindings", '\0', "report-bindings", "report-bindings", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Whether to report process bindings to stderr" },
-
-    /* slot list option */
-    { "hwloc_base_slot_list", '\0', "slot-list", "slot-list", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "List of processor IDs to bind processes to [default=NULL]"},
-
-    /* generalized pattern mapping option */
-    { "rmaps_ppr_pattern", '\0', NULL, "ppr", 1,
-        NULL, OPAL_CMD_LINE_TYPE_STRING,
-        "Comma-separated list of number of processes on a given resource type [default: none]" },
-#else
-    /* Mapping options */
-    { "rmaps_base_mapping_policy", '\0', NULL, "map-by", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Mapping Policy [slot (default) | node]" },
-
-      /* Ranking options */
-    { "rmaps_base_ranking_policy", '\0', NULL, "rank-by", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Ranking Policy [slot (default) | node]" },
-#endif
-
-    /* Allocation options */
-    { "orte_display_alloc", '\0', "display-allocation", "display-allocation", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Display the allocation being used by this job"},
-    { "orte_display_devel_alloc", '\0', "display-devel-allocation", "display-devel-allocation", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Display a detailed list (mostly intended for developers) of the allocation being used by this job"},
-#if OPAL_HAVE_HWLOC
-    { "hwloc_base_cpu_set", '\0', "cpu-set", "cpu-set", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Comma-separated list of ranges specifying logical cpus allocated to this job [default: none]"},
-#endif
-    { NULL, 'H', "host", "host", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "List of hosts to invoke processes on" },
-
-    /* mpiexec-like arguments */
-    { NULL, '\0', "wdir", "wdir", 1,
-      &orun_globals.wdir, OPAL_CMD_LINE_TYPE_STRING,
-      "Set the working directory of the started processes" },
-    { NULL, '\0', "wd", "wd", 1,
-      &orun_globals.wdir, OPAL_CMD_LINE_TYPE_STRING,
-      "Synonym for --wdir" },
-    { NULL, '\0', "set-cwd-to-session-dir", "set-cwd-to-session-dir", 0,
-      &orun_globals.set_cwd_to_session_dir, OPAL_CMD_LINE_TYPE_BOOL,
-      "Set the working directory of the started processes to their session directory" },
     { NULL, '\0', "path", "path", 1,
       &orun_globals.path, OPAL_CMD_LINE_TYPE_STRING,
       "PATH to be used to look for executables to start processes" },
 
-    /* User-level debugger arguments */
-    { NULL, '\0', "tv", "tv", 0,
-      &orun_globals.debugger, OPAL_CMD_LINE_TYPE_BOOL,
-      "Deprecated backwards compatibility flag; synonym for \"--debug\"" },
-    { NULL, '\0', "debug", "debug", 0,
-      &orun_globals.debugger, OPAL_CMD_LINE_TYPE_BOOL,
-      "Invoke the user-level debugger indicated by the orte_base_user_debugger MCA parameter" },
-    { "orte_base_user_debugger", '\0', "debugger", "debugger", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Sequence of debuggers to search for when \"--debug\" is used" },
-    { "orte_output_debugger_proctable", '\0', "output-proctable", "output-proctable", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Output the debugger proctable after launch" },
-    
     /* OpenRTE arguments */
-    { "orte_debug", 'd', "debug-devel", "debug-devel", 0,
+    { "orte_debug", 'd', "debug", "debug", 0,
       NULL, OPAL_CMD_LINE_TYPE_BOOL,
       "Enable debugging of OpenRTE" },
     
@@ -435,62 +162,9 @@ static opal_cmd_line_init_t cmd_line_init[] = {
       NULL, OPAL_CMD_LINE_TYPE_BOOL,
       "Enable debugging of any OpenRTE daemons used by this application, storing output in files" },
     
-    { "orte_leave_session_attached", '\0', "leave-session-attached", "leave-session-attached", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Enable debugging of OpenRTE" },
-
-    { "orte_do_not_launch", '\0', "do-not-launch", "do-not-launch", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Perform all necessary operations to prepare to launch the application, but do not actually launch it" },
-    
-    { NULL, '\0', NULL, "prefix", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Prefix where Open MPI is installed on remote nodes" },
-    { NULL, '\0', NULL, "noprefix", 0,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Disable automatic --prefix behavior" },
-
-    { "orte_report_launch_progress", '\0', "show-progress", "show-progress", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Output a brief periodic report on launch progress" },
-
-    { "orte_use_regexp", '\0', "use-regexp", "use-regexp", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Use regular expressions for launch" },
-
-    { "orte_report_events", '\0', "report-events", "report-events", 1,
-      NULL, OPAL_CMD_LINE_TYPE_STRING,
-      "Report events to a tool listening at the specified URI" },
-
-    { "orte_enable_recovery", '\0', "enable-recovery", "enable-recovery", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Enable recovery from process failure [Default = disabled]" },
-
-    { "orte_max_restarts", '\0', "max-restarts", "max-restarts", 1,
-      NULL, OPAL_CMD_LINE_TYPE_INT,
-      "Max number of times to restart a failed process" },
-
-#if OPAL_HAVE_HWLOC
-    { "orte_hetero_nodes", '\0', NULL, "hetero-nodes", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Nodes in cluster may differ in topology, so send the topology back from each node [Default = false]" },
-#endif
-
-    { NULL, '\0', "disable-recovery", "disable-recovery", 0,
-      &orun_globals.disable_recovery, OPAL_CMD_LINE_TYPE_BOOL,
-      "Disable recovery (resets all recovery options to off)" },
-
-    { "state_novm_select", '\0', "novm", "novm", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Execute without creating an allocation-spanning virtual machine (only start daemons on nodes hosting application procs)" },
-
-    { "orte_staged_execution", '\0', "staged", "staged", 0,
-      NULL, OPAL_CMD_LINE_TYPE_BOOL,
-      "Used staged execution if inadequate resources are present (cannot support MPI jobs)" },
-
     /* Scheduler options for new allocation */
     { NULL,
-      'a', NULL, "account",
+      '\0', NULL, "account",
       1,
       &orun_globals.account, OPAL_CMD_LINE_TYPE_STRING,
       "Account to be charged" },
@@ -502,64 +176,28 @@ static opal_cmd_line_init_t cmd_line_init[] = {
       "User assigned project name" },
 
     { NULL,
-      'g', NULL, "gid",
+      '\0', NULL, "gid",
       1,
       &orun_globals.gid, OPAL_CMD_LINE_TYPE_INT,
       "Group id to run session under" },
 
     { NULL,
-      'N', NULL, "max-node",
+      '\0', NULL, "max-node",
       1,
       &orun_globals.max_nodes, OPAL_CMD_LINE_TYPE_INT,
       "Max nodes allowed in allocation" },
 
     { NULL,
-      'P', NULL, "max-pe",
-      1,
-      &orun_globals.max_pes, OPAL_CMD_LINE_TYPE_INT,
-      "Max PEs allowed in allocation" },
-
-    { NULL,
-      'M', NULL, "node",
+      '\0', NULL, "min-node",
       1,
       &orun_globals.min_nodes, OPAL_CMD_LINE_TYPE_INT,
       "Minimum number of nodes required for allocation" },
 
     { NULL,
-      'p', NULL, "pe",
+      '\0', NULL, "node",
       1,
-      &orun_globals.min_pes, OPAL_CMD_LINE_TYPE_INT,
-      "Minimum number of PEs required for allocation" },
-
-    { NULL,
-      's', NULL, "start",
-      1,
-      &orun_globals.starttime, OPAL_CMD_LINE_TYPE_STRING,
-      "Earliest Date/Time required to start job" },
-
-    { NULL,
-      'w', NULL, "walltime",
-      1,
-      &orun_globals.walltime, OPAL_CMD_LINE_TYPE_STRING,
-      "Maximum duration before job is terminated" },
-
-    { NULL,
-      'e', NULL, "exclusive",
-      0,
-      &orun_globals.exclusive, OPAL_CMD_LINE_TYPE_BOOL,
-      "Do not share allocated nodes with other sessions" },
-
-    { NULL,
-      'f', NULL, "nodefile",
-      0,
-      &orun_globals.nodefile, OPAL_CMD_LINE_TYPE_STRING,
-      "Path to file listing names of candidate nodes" },
-
-    { NULL,
-      'c', NULL, "constraints",
-      1,
-      &orun_globals.resource, OPAL_CMD_LINE_TYPE_STRING,
-      "Resource constraints to be applied" },
+      &orun_globals.min_nodes, OPAL_CMD_LINE_TYPE_INT,
+      "Minimum number of nodes required for allocation" },
 
     {"orte_hnp_uri", '\0', "hnp-uri", "hnp-uri", 1,
       &my_hnp_uri, OPAL_CMD_LINE_TYPE_STRING,

0ac3b23aa1627ccc88d22f3488b69ce663e89727
lrrajesh <l.r.rajeshnarayanan@intel.com>
2014-09-12 07:57:49 -0700

    Copyright header changes for orun and orcmsd

--- orcm/tools/orcmsd/orcmsd.c
+++ orcm/tools/orcmsd/orcmsd.c
@@ -12,6 +12,7 @@
  * Copyright (c) 2007      Cisco Systems, Inc.  All rights reserved.
  * Copyright (c) 2007      Los Alamos National Security, LLC.  All rights
  *                         reserved. 
+ * Copyright (c) 2014      Intel, Inc.  All rights reserved. 
  * $COPYRIGHT$
  *
  * Additional copyrights may follow

d8197105ce839d102176f516dcf6b74e7535839d
lrrajesh <l.r.rajeshnarayanan@intel.com>
2014-09-23 16:58:36 -0700

    Batch Job launch implementation

--- orcm/mca/scd/base/scd_dt_fns.c
+++ orcm/mca/scd/base/scd_dt_fns.c
@@ -256,6 +256,24 @@ int orcm_pack_alloc(opal_buffer_t *buffer, const void *src,
             ORTE_ERROR_LOG(ret);
             return ret;
         }
+        /* pack the batch file */
+        if (OPAL_SUCCESS !=
+            (ret = opal_dss_pack_buffer(buffer,
+                                        (void*)&alloc->batchfile,
+                                        1,
+                                        OPAL_STRING))) {
+            ORTE_ERROR_LOG(ret);
+            return ret;
+        }
+        /* pack the batch flag */
+        if (OPAL_SUCCESS !=
+            (ret = opal_dss_pack_buffer(buffer,
+                                        (void*)&alloc->batch,
+                                        1,
+                                        OPAL_BOOL))) {
+            ORTE_ERROR_LOG(ret);
+            return ret;
+        }
         /* pack the resource constraints */
         j = (int32_t)opal_list_get_size(&alloc->constraints);
             if (OPAL_SUCCESS !=
@@ -551,6 +569,26 @@ int orcm_unpack_alloc(opal_buffer_t *buffer, void *dest,
                                           &a->queues,
                                           &n,
                                           OPAL_STRING))) {
+            ORTE_ERROR_LOG(ret);
+            return ret;
+        }
+        /* unpack the batch file */
+        n=1;
+        if (OPAL_SUCCESS !=
+            (ret = opal_dss_unpack_buffer(buffer,
+                                          &a->batchfile,
+                                          &n,
+                                          OPAL_STRING))) {
+            ORTE_ERROR_LOG(ret);
+            return ret;
+        }
+        /* unpack the batch flag */
+        n=1;
+        if (OPAL_SUCCESS !=
+            (ret = opal_dss_unpack_buffer(buffer,
+                                          &a->batch,
+                                          &n,
+                                          OPAL_BOOL))) {
             ORTE_ERROR_LOG(ret);
             return ret;
         }

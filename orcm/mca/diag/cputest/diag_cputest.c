d12bdc56d6e596d97b6c69b4f72deffb62619d62
yhuan47 <yongkang.huang@intel.com>
2014-09-26 15:52:54 -0700

    Add basic cpu diag plugin, test modules to be implemented.

---
+++ orcm/mca/diag/cputest/diag_cputest.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2014      Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "orcm_config.h"
+#include "orcm/constants.h"
+
+#include <errno.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif  /* HAVE_UNISTD_H */
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif  /* HAVE_STRING_H */
+#ifdef HAVE_DIRENT_H
+#include <dirent.h>
+#endif  /* HAVE_DIRENT_H */
+#include <ctype.h>
+
+#include <sys/mman.h>
+#include <sys/sysinfo.h>
+#include <sys/resource.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "opal/class/opal_list.h"
+#include "opal/util/argv.h"
+#include "opal/util/output.h"
+#include "opal/util/os_dirpath.h"
+#include "opal/util/os_path.h"
+#include "opal/mca/installdirs/installdirs.h"
+#include "opal/mca/hwloc/hwloc.h"
+
+#include "orte/mca/errmgr/errmgr.h"
+
+#include "orcm/mca/db/db.h"
+#include "orcm/mca/sensor/base/sensor_private.h"
+
+#include "orcm/mca/diag/base/base.h"
+#include "diag_cputest.h"
+
+static int init(void);
+static void finalize(void);
+static int diag_read(opal_list_t *config);
+static int diag_check(opal_list_t *config);
+
+static int mem_diag_ret = ORCM_SUCCESS;
+
+orcm_diag_base_module_t orcm_diag_cputest_module = {
+    init,
+    finalize,
+    NULL,
+    diag_read,
+    diag_check
+};
+
+static int init(void)
+{
+    OPAL_OUTPUT_VERBOSE((5, orcm_diag_base_framework.framework_output,
+                         "%s diag:cputest:init",
+                         ORTE_NAME_PRINT(ORTE_PROC_MY_NAME)));
+    return ORCM_SUCCESS;
+
+}
+
+static void finalize(void)
+{
+    OPAL_OUTPUT_VERBOSE((5, orcm_diag_base_framework.framework_output,
+                         "%s diag:cputest:finalize",
+                         ORTE_NAME_PRINT(ORTE_PROC_MY_NAME)));
+    return;
+}
+
+
+static int diag_read(opal_list_t *config)
+{
+    return ORCM_SUCCESS;
+}
+
+static int diag_check(opal_list_t *config)
+{
+
+    return mem_diag_ret;
+}
+
+

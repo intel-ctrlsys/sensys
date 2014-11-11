2326ba906701181c29c32fcc7b4f0c250f015e47
Alejandro Vilches <alejandro.vilches@intel.com>
2014-11-11 15:30:38 -0800

    Updated the orcmdb schema creation script for PostgreSQL.

--- contrib/database/orcmdb_psql.sql
+++ contrib/database/orcmdb_psql.sql
@@ -26,22 +26,261 @@ SET client_min_messages = warning;
 SET search_path = public, pg_catalog;
 
 --
--- Name: add_data_sample(character varying, character varying, character varying, timestamp without time zone, double precision, character varying, character varying); Type: FUNCTION; Schema: public; Owner: -
+-- Name: add_data_item(character varying, integer); Type: FUNCTION; Schema: public; Owner: -
 --
 
-CREATE FUNCTION add_data_sample(p_hostname character varying, p_data_group character varying, p_data_item character varying, p_time_stamp timestamp without time zone, p_value_num double precision, p_value_str character varying, p_units character varying) RETURNS void
+CREATE FUNCTION add_data_item(p_data_item character varying, p_data_type integer) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+    v_data_item_id integer := 0;
+
+BEGIN
+    insert into data_item(
+        name,
+        data_type)
+    values(
+        p_data_item,
+        p_data_type);
+
+    select data_item_id into v_data_item_id
+    from data_item
+    where name = p_data_item;
+
+    return v_data_item_id;
+END
+
+$$;
+
+
+--
+-- Name: add_data_sample(integer, integer, timestamp without time zone, double precision, character varying, character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION add_data_sample(p_node_id integer, p_data_item_id integer, p_time_stamp timestamp without time zone, p_value_num double precision, p_value_str character varying, p_units character varying) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    insert into data_sample(
+        node_id,
+        data_item_id,
+        time_stamp,
+        value_num,
+        value_str,
+        units)
+    values(
+        p_node_id,
+        p_data_item_id,
+        p_time_stamp,
+        p_value_num,
+        p_value_str,
+        p_units);
+
+    return;
+END
+$$;
+
+
+--
+-- Name: add_feature(character varying, integer); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION add_feature(p_feature character varying, p_data_type integer) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+    v_feature_id integer := 0;
+
+BEGIN
+    insert into feature(
+        name,
+        data_type)
+    values(
+        p_feature,
+        p_data_type);
+
+    select feature_id into v_feature_id
+    from feature
+    where name = p_feature;
+
+    return v_feature_id;
+END
+
+$$;
+
+
+--
+-- Name: add_node(character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION add_node(p_hostname character varying) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+    v_node_id integer := 0;
+
+BEGIN
+    insert into node(hostname)
+    values(p_hostname);
+
+    select node_id into v_node_id
+    from node
+    where hostname = p_hostname;
+
+    return v_node_id;
+END
+
+$$;
+
+
+--
+-- Name: add_node_feature(integer, integer, double precision, character varying, character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION add_node_feature(p_node_id integer, p_feature_id integer, p_value_num double precision, p_value_str character varying, p_units character varying) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    insert into node_feature(
+        node_id,
+        feature_id,
+        value_num,
+        value_str,
+        units)
+    values(
+        p_node_id,
+        p_feature_id,
+        p_value_num,
+        p_value_str,
+        p_units);
+
+    return;
+END
+$$;
+
+
+--
+-- Name: get_data_item_id(character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION get_data_item_id(p_data_item character varying) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+    v_data_item_id integer := 0;
+
+BEGIN
+    -- Get the data item ID
+    select data_item_id into v_data_item_id
+    from data_item
+    where name = p_data_item;
+
+    -- Does it exist?
+    if (v_data_item_id is null) then
+        v_data_item_id := 0;
+    end if;
+
+    return v_data_item_id;
+END
+$$;
+
+
+--
+-- Name: get_feature_id(character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION get_feature_id(p_feature character varying) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+    v_feature_id integer := 0;
+
+BEGIN
+    -- Get the feature ID
+    select feature_id into v_feature_id
+    from feature
+    where name = p_feature;
+
+    -- Does it exist?
+    if (v_feature_id is null) then
+        v_feature_id := 0;
+    end if;
+
+    return v_feature_id;
+END
+$$;
+
+
+--
+-- Name: get_node_id(character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION get_node_id(p_hostname character varying) RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+    v_node_id integer := 0;
+
+BEGIN
+    -- Get the node ID
+    select node_id into v_node_id
+    from node
+    where hostname = p_hostname;
+
+    -- Does it exist?
+    if (v_node_id is null) then
+        v_node_id := 0;
+    end if;
+
+    return v_node_id;
+END
+
+$$;
+
+
+--
+-- Name: node_feature_exists(integer, integer); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION node_feature_exists(p_node_id integer, p_feature_id integer) RETURNS boolean
     LANGUAGE plpgsql
     AS $$
 DECLARE
-    v_node_id numeric;
-    v_data_item_id numeric;
-    v_data_item_name varchar(250);
-    v_data_type numeric;
+    v_exists boolean := false;
 
 BEGIN
-    -- Initialize variables
-    v_node_id := 0;
-    v_data_item_id := 0;
+
+    v_exists := exists(
+        select 1
+        from node_feature
+        where node_id = p_node_id and feature_id = p_feature_id);
+
+    return v_exists;
+END
+$$;
+
+
+--
+-- Name: record_data_sample(character varying, character varying, character varying, timestamp without time zone, double precision, character varying, character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION record_data_sample(p_hostname character varying, p_data_group character varying, p_data_item character varying, p_time_stamp timestamp without time zone, p_value_num double precision, p_value_str character varying, p_units character varying) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    v_node_id integer := 0;
+    v_data_item_id integer := 0;
+    v_data_item_name varchar(250);
+    v_data_type integer := 0;
+
+BEGIN
+    -- Compose the data item name from the data group and item itself
     v_data_item_name := p_data_group || '_';
     v_data_item_name := v_data_item_name || p_data_item;
 
@@ -56,73 +295,112 @@ BEGIN
     end if;
 
     -- Get the node ID
-    select node_id into v_node_id
-    from node
-    where hostname = p_hostname;
-
-    -- Add the node if it doesn't exist
-    if (v_node_id = 0 or v_node_id is null) then
-        insert into node(
-            hostname,
-            node_type,
-            status,
-            num_cpus,
-            num_sockets,
-            cores_per_socket,
-            threads_per_core,
-            memory)
-        values(
-            p_hostname,
-            NULL,
-            NULL,
-            NULL,
-            NULL,
-            NULL,
-            NULL,
-            NULL);
-
-        select node_id into v_node_id
-        from node
-        where hostname = p_hostname;
+    v_node_id := get_node_id(p_hostname);
+    if (v_node_id = 0) then
+        v_node_id := add_node(p_hostname);
     end if;
 
     -- Get the data item ID
-    select data_item_id into v_data_item_id
-    from data_item
-    where name = v_data_item_name;
-
-    -- Add the data item if it doesn't exist
-    if (v_data_item_id = 0 or v_data_item_id is null) then
-        insert into data_item(
-            name,
-            data_type)
-        values(
-            v_data_item_name,
-            v_data_type);
-
-        select data_item_id into v_data_item_id
-        from data_item
-        where name = v_data_item_name;
+    v_data_item_id := get_data_item_id(v_data_item_name);
+    if (v_data_item_id = 0) then
+        v_data_item_id := add_data_item(v_data_item_name, v_data_type);
     end if;
 
     -- Add the data sample
-    insert into data_sample(
-        node_id,
-        data_item_id,
-        time_stamp,
-        value_num,
-        value_str,
-        units)
-    values(
+    perform add_data_sample(
         v_node_id,
         v_data_item_id,
         p_time_stamp,
         p_value_num,
-        p_value_str,
-        p_units);
+        p_value_str, p_units);
 
     return;
 END;
+$$;
+
+
+--
+-- Name: set_node_feature(character varying, character varying, double precision, character varying, character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION set_node_feature(p_hostname character varying, p_feature character varying, p_value_num double precision, p_value_str character varying, p_units character varying) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+
+DECLARE
+    v_node_id integer := 0;
+    v_feature_id integer := 0;
+    v_data_type integer := 0;
+    v_node_feature_exists boolean := true;
+
+BEGIN
+    -- Determine the value type
+    if (p_value_num is not null and p_value_str is null) then
+        v_data_type := 1; -- Numeric data type
+    elseif (p_value_str is not null and p_value_num is null) then
+        v_data_type := 2; -- String data type
+    else
+        raise exception 'No value passed or ambiguous value defined';
+        raise SQLSTATE '45000';
+    end if;
+
+    -- Get the node ID
+    v_node_id := get_node_id(p_hostname);
+    if (v_node_id = 0) then
+        v_node_id := add_node(p_hostname);
+        v_node_feature_exists := false;
+    end if;
+
+    -- Get the feature ID
+    v_feature_id := get_feature_id(p_feature);
+    if (v_feature_id = 0) then
+        v_feature_id := add_feature(p_feature, v_data_type);
+        v_node_feature_exists := false;
+    end if;
+
+    -- Has this feature already been added for this node?
+    if (v_node_feature_exists) then
+        v_node_feature_exists := node_feature_exists(v_node_id, v_feature_id);
+    end if;
+
+    if (v_node_feature_exists) then
+        -- If so, just update it
+        perform update_node_feature(
+            v_node_id,
+            v_feature_id,
+            p_value_num,
+            p_value_str,
+            p_units);
+    else        
+        -- If not, add it
+        perform add_node_feature(
+            v_node_id,
+            v_feature_id,
+            p_value_num,
+            p_value_str,
+            p_units);
+    end if;
+
+    return;
+END
+
+$$;
+
+
+--
+-- Name: update_node_feature(integer, integer, double precision, character varying, character varying); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION update_node_feature(p_node_id integer, p_feature_id integer, p_value_num double precision, p_value_str character varying, p_units character varying) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    update node_feature
+    set value_num = p_value_num,
+        value_str = p_value_str,
+        units = p_units
+    where node_id = p_node_id and feature_id = p_feature_id;
+END
 $$;
 
 
@@ -171,7 +449,8 @@ CREATE TABLE data_sample (
     time_stamp timestamp without time zone NOT NULL,
     value_num double precision,
     value_str character varying(50),
-    units character varying(50)
+    units character varying(50),
+    CONSTRAINT value CHECK (((value_num IS NOT NULL) OR (value_str IS NOT NULL)))
 );
 
 
@@ -197,22 +476,18 @@ CREATE TABLE node (
 --
 
 CREATE VIEW data_samples_view AS
- SELECT ( SELECT node.hostname
-           FROM node) AS hostname,
-    ( SELECT data_item.name
-           FROM data_item) AS name,
-    ( SELECT data_item.data_type
-           FROM data_item) AS data_type,
-    ( SELECT data_sample.value_num
-           FROM data_sample) AS value_num,
-    ( SELECT data_sample.units
-           FROM ((data_sample
-      JOIN node ON ((( SELECT node_1.node_id
-              FROM node node_1) = ( SELECT data_sample_1.node_id
-              FROM data_sample data_sample_1))))
-   JOIN data_item ON ((( SELECT data_item_1.data_item_id
-         FROM data_item data_item_1) = ( SELECT data_sample_1.data_item_id
-         FROM data_sample data_sample_1))))) AS units;
+ SELECT data_sample.node_id,
+    node.hostname,
+    data_sample.data_item_id,
+    data_item.name AS data_item,
+    data_item.data_type,
+    data_sample.time_stamp,
+    data_sample.value_num,
+    data_sample.value_str,
+    data_sample.units
+   FROM ((data_sample
+     JOIN node ON ((node.node_id = data_sample.node_id)))
+     JOIN data_item ON ((data_item.data_item_id = data_sample.data_item_id)));
 
 
 --
@@ -254,6 +529,37 @@ CREATE SEQUENCE event_type_event_type_id_seq
 --
 
 ALTER SEQUENCE event_type_event_type_id_seq OWNED BY event_type.event_type_id;
+
+
+--
+-- Name: feature; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE feature (
+    feature_id integer NOT NULL,
+    name character varying(250) NOT NULL,
+    data_type integer DEFAULT 1 NOT NULL,
+    CONSTRAINT data_type CHECK (((data_type = 1) OR (data_type = 2)))
+);
+
+
+--
+-- Name: feature_feature_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE feature_feature_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: feature_feature_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE feature_feature_id_seq OWNED BY feature.feature_id;
 
 
 --
@@ -377,6 +683,20 @@ CREATE TABLE maintenance_record (
 
 
 --
+-- Name: node_feature; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE node_feature (
+    node_id integer NOT NULL,
+    feature_id integer NOT NULL,
+    value_num double precision,
+    value_str character varying(100),
+    units character varying(50),
+    CONSTRAINT value CHECK (((value_num IS NOT NULL) OR (value_str IS NOT NULL)))
+);
+
+
+--
 -- Name: node_node_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -407,6 +727,13 @@ ALTER TABLE ONLY data_item ALTER COLUMN data_item_id SET DEFAULT nextval('data_i
 --
 
 ALTER TABLE ONLY event_type ALTER COLUMN event_type_id SET DEFAULT nextval('event_type_event_type_id_seq'::regclass);
+
+
+--
+-- Name: feature_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY feature ALTER COLUMN feature_id SET DEFAULT nextval('feature_feature_id_seq'::regclass);
 
 
 --
@@ -470,6 +797,22 @@ ALTER TABLE ONLY event_type
 
 
 --
+-- Name: feature_name_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY feature
+    ADD CONSTRAINT feature_name_key UNIQUE (name);
+
+
+--
+-- Name: feature_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY feature
+    ADD CONSTRAINT feature_pkey PRIMARY KEY (feature_id);
+
+
+--
 -- Name: fru_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -507,6 +850,14 @@ ALTER TABLE ONLY job
 
 ALTER TABLE ONLY maintenance_record
     ADD CONSTRAINT maintenance_record_pkey PRIMARY KEY (node_id, fru_type_id, fru_id, replacement_date);
+
+
+--
+-- Name: node_feature_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY node_feature
+    ADD CONSTRAINT node_feature_pkey PRIMARY KEY (node_id, feature_id);
 
 
 --
@@ -603,6 +954,32 @@ ALTER TABLE ONLY job_node
 
 ALTER TABLE ONLY maintenance_record
     ADD CONSTRAINT maintenance_record_node_id_fkey FOREIGN KEY (node_id, fru_type_id, fru_id) REFERENCES fru(node_id, fru_type_id, fru_id);
+
+
+--
+-- Name: node_feature_feature_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY node_feature
+    ADD CONSTRAINT node_feature_feature_id_fkey FOREIGN KEY (feature_id) REFERENCES feature(feature_id);
+
+
+--
+-- Name: node_feature_node_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY node_feature
+    ADD CONSTRAINT node_feature_node_id_fkey FOREIGN KEY (node_id) REFERENCES node(node_id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: -
+--
+
+-- REVOKE ALL ON SCHEMA public FROM PUBLIC;
+-- REVOKE ALL ON SCHEMA public FROM postgres;
+-- GRANT ALL ON SCHEMA public TO postgres;
+-- GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --

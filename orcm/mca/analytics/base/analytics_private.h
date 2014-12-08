1c15b1f40829a96febf7ab25e35fd4aed1ff2364
Ben McClelland <benjamin.m.mcclelland@intel.com>
2014-12-08 15:22:24 -0800

    minor fixes for rpm builds

--- orcm/mca/analytics/base/analytics_private.h
+++ orcm/mca/analytics/base/analytics_private.h
@@ -15,11 +15,11 @@
 
 BEGIN_C_DECLS
 
-int orcm_analytics_base_workflow_create(opal_buffer_t* buffer, int *wfid);
-int orcm_analytics_base_workflow_delete(int workflow_id);
-int orcm_analytics_base_comm_start(void);
-int orcm_analytics_base_comm_stop(void);
-int orcm_analytics_base_select_workflow_step(orcm_workflow_step_t *workflow);
+ORCM_DECLSPEC int orcm_analytics_base_workflow_create(opal_buffer_t* buffer, int *wfid);
+ORCM_DECLSPEC int orcm_analytics_base_workflow_delete(int workflow_id);
+ORCM_DECLSPEC int orcm_analytics_base_comm_start(void);
+ORCM_DECLSPEC int orcm_analytics_base_comm_stop(void);
+ORCM_DECLSPEC int orcm_analytics_base_select_workflow_step(orcm_workflow_step_t *workflow);
 
 END_C_DECLS
 #endif

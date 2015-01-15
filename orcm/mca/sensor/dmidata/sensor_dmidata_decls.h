bdbe330bb0be5bf59d547efd1f3ee4b864c2bdb6
Vineet Pedaballe <vineet.pedaballe@intel.com>
2015-01-15 13:21:29 -0800

    Enabled testvector for testing inventory data in IPMI plugin

--- orcm/mca/sensor/dmidata/sensor_dmidata_decls.h
+++ orcm/mca/sensor/dmidata/sensor_dmidata_decls.h
@@ -16,7 +16,7 @@
 #define ORCM_SENSOR_DMIDATA_DECLS_H
 
 #define MAX_INVENTORY_KEYWORDS      12
-#define MAX_INVENTORY_SUB_KEYWORDS  5  /* 3 for search, 1 for ignore and 1 for naming the inventory item */
+#define MAX_INVENTORY_SUB_KEYWORDS  6  /* 3 for search, 1 for ignore, 1 for naming the inventory item, and 1 for storing TEST_VECTOR */
 #define MAX_INVENTORY_KEYWORD_SIZE  30
 
 typedef struct {

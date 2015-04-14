e71367f2db7ec20b925d351d3757f5ad3f9247d4
lrrajesh <l.r.rajeshnarayanan@intel.com>
2015-04-14 14:56:23 -0700

    freq perthread sampling

--- orcm/mca/sensor/freq/sensor_freq_component.c
+++ orcm/mca/sensor/freq/sensor_freq_component.c
@@ -104,5 +104,21 @@ static int freq_component_register(void)
                                             MCA_BASE_VAR_SCOPE_READONLY,
                                             & mca_sensor_freq_component.pstate);
 
+    mca_sensor_freq_component.use_progress_thread = false;
+    (void) mca_base_component_var_register(c, "use_progress_thread",
+                                           "Use a dedicated progress thread for freq sensors [default: false]",
+                                           MCA_BASE_VAR_TYPE_BOOL, NULL, 0, 0,
+                                           OPAL_INFO_LVL_9,
+                                           MCA_BASE_VAR_SCOPE_READONLY,
+                                           &mca_sensor_freq_component.use_progress_thread);
+
+    mca_sensor_freq_component.sample_rate = 0;
+    (void) mca_base_component_var_register(c, "sample_rate",
+                                           "Sample rate in seconds",
+                                           MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
+                                           OPAL_INFO_LVL_9,
+                                           MCA_BASE_VAR_SCOPE_READONLY,
+                                           &mca_sensor_freq_component.sample_rate);
+  
     return ORCM_SUCCESS;
 }

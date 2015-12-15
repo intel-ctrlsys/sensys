4eb1301fbc74167b6249a59ef24487ebfafaeaee
Paul Amonson <paul.d.amonson@intel.com>
2015-12-15 14:49:33 -0500

    Fixed a missed copyright in opal/class/object.h
    Change-Id: If10755ca2718ac159e4ec4798a3e45d62beb84be
    Signed-off-by: Paul Amonson <paul.d.amonson@intel.com>

--- opal/class/opal_object.h
+++ opal/class/opal_object.h
@@ -10,6 +10,7 @@
  * Copyright (c) 2004-2005 The Regents of the University of California.
  *                         All rights reserved.
  * Copyright (c) 2007-2014 Cisco Systems, Inc.  All rights reserved.
+ * Copyright (c) 2015      Intel, Inc. All rights reserved.
  * $COPYRIGHT$
  * Additional copyrights may follow
  * $HEADER$

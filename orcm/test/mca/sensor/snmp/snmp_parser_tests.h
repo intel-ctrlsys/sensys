d3ebebb74455e6407b60b1314c2d2b4cff46c6b7
Victor Escamilla <victor.escamilla.ochoa@intel.com>
2016-01-07 16:27:01 -0600

    Fixes for snmp functional tests.
    Change-Id: I588e98cffcef866fa320ab6c70f50342d1746bc2
    Signed-off-by: Victor Escamilla <victor.escamilla.ochoa@intel.com>

--- orcm/test/mca/sensor/snmp/snmp_parser_tests.h
+++ orcm/test/mca/sensor/snmp/snmp_parser_tests.h
@@ -16,11 +16,19 @@ extern "C"{
     #include "opal/runtime/opal.h"
 }
 #include <string.h>
+#include <iostream>
 
 class ut_snmp_parser_tests: public testing::Test {
-
     protected:
+        string test_files_dir;
         ut_snmp_parser_tests(){
+            // 'make check' will provide $(srcdir) with the path to
+            // test source folder.
+            char* srcdir = getenv("srcdir");
+            if (NULL != srcdir)
+                test_files_dir = string(srcdir) + "/test_files/";
+            else
+                test_files_dir = "test_files/";
         }
 
         virtual void SetUp(){

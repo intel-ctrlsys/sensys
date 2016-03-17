ff53dc069053100858a8c02325b8a8bb8f723c54
lrrajesh <l.r.rajeshnarayanan@intel.com>
2016-03-16 21:16:53 -0700

    Disable sampling for sensor init fails Signed-off-by: lrrajesh <l.r.rajeshnarayanan@intel.com>
    Change-Id: I4e9367aa7977e3882b56059c97b18a62f0c17aeb

---
+++ orcm/test/mca/data_dispatch/data_dispatch/dfg/data_dispatch_dfg_tests.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2015      Intel, Inc. All rights reserved.*/
+
+#ifndef GREI_ORCM_TEST_MCA_EVGEN_SAEG_EVGEN_SAEG_TEST_H
+#define GREI_ORCM_TEST_MCA_EVGEN_SAEG_EVGEN_SAEG_TEST_H
+
+#include "gtest/gtest.h"
+
+extern "C" {
+    #include "orcm/mca/analytics/base/analytics_private.h"
+    #include "orcm/mca/db/base/base.h"
+    #include "opal/class/opal_list.h"
+    #include "opal/mca/event/external/external.h"
+    #include "orcm/mca/data_dispatch/dfg/data_dispatch_dfg.h"
+    #include "opal/mca/event/event.h"
+};
+
+#endif

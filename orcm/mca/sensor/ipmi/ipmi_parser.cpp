0ef82de2ae81da062a6c31831222f3763c6b32f7
Minia Oseguera <minia.oseguera@intel.com>
2016-04-07 09:51:55 -0500

    [SENSOR] Fixed typo in Ipmi Parser for BMC Address Regex
    Change-Id: I16caa67bd55e9ef1c6347e36e1dd6f6d75a9d714
    Signed-off-by: Minia Oseguera <minia.oseguera@intel.com>

--- orcm/mca/sensor/ipmi/ipmi_parser.cpp
+++ orcm/mca/sensor/ipmi/ipmi_parser.cpp
@@ -13,7 +13,7 @@
 #include <regex.h>
 
 #define IP_ADDRESS_REGEX \
-    "^(([0-9]|[1-9][1-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])[.]){3}([0-9]|[1-9][1-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+    "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])[.]){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 
 #define getStringValueFromItem(variable, searchedKey, field) \
     if (NULL != field && OPAL_STRING == field->value.type    \

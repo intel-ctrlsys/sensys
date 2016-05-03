3459fcc0317f83355ad700a3000426d426d7a019
Paul Amonson <paul.d.amonson@intel.com>
2016-05-03 14:48:33 -0700

    Extending code coverage tests for mcedata.
    Change-Id: I0cb2e0b2bc63723d91475ea65d1da4a323dbd328
    Signed-off-by: Paul Amonson <paul.d.amonson@intel.com>

--- orcm/test/mca/sensor/mcedata/mcedata_tests.cpp
+++ orcm/test/mca/sensor/mcedata/mcedata_tests.cpp
@@ -8,14 +8,19 @@
  */
 
 #include "mcedata_tests.h"
+#include "mcedata_tests_mocking.h"
 
 // ORTE
 #include "orte/runtime/orte_globals.h"
 
 // ORCM
+#include "orcm/mca/mca.h"
+#include "orcm/mca/sensor/sensor.h"
+#include "orcm/mca/sensor/base/base.h"
 #include "orcm/mca/sensor/base/sensor_private.h"
 #include "orcm/mca/sensor/base/sensor_runtime_metrics.h"
 #include "orcm/mca/sensor/mcedata/sensor_mcedata.h"
+
 
 // Fixture
 using namespace std;
@@ -26,6 +31,11 @@ extern "C" {
     extern int mcedata_enable_sampling(const char* sensor_specification);
     extern int mcedata_disable_sampling(const char* sensor_specification);
     extern int mcedata_reset_sampling(const char* sensor_specification);
+    extern void mcedata_unknown_filter(unsigned long *mce_reg, opal_list_t *vals);
+    extern void mcedata_gen_cache_filter(unsigned long *mce_reg, opal_list_t *vals);
+    extern void mcedata_tlb_filter(unsigned long *mce_reg, opal_list_t *vals);
+    extern void mcedata_cache_filter(unsigned long *mce_reg, opal_list_t *vals);
+    extern void mcedata_bus_ic_filter(unsigned long *mce_reg, opal_list_t *vals);
 };
 
 void ut_mcedata_tests::SetUpTestCase()
@@ -39,6 +49,53 @@ void ut_mcedata_tests::TearDownTestCase()
 {
     // Release OPAL level resources
     opal_dss_close();
+}
+
+
+DIR* ut_mcedata_tests::OpenDir(const char* dirname)
+{
+    (void)dirname;
+    int* rv = new int;
+    *rv = 0;
+    return (DIR*)rv;
+}
+
+int ut_mcedata_tests::CloseDir(DIR* dir_fd)
+{
+    delete (int*)dir_fd;
+    return 0;
+}
+
+static const char* entries[] = {
+    ".",
+    "..",
+    "mcelog",
+    (const char*)NULL
+};
+
+struct dirent* ut_mcedata_tests::ReadDir(DIR* dir_fd)
+{
+    static struct dirent dir;
+    memset(&dir, 0, sizeof(struct dirent));
+    int* index = (int*)dir_fd;
+    if(entries[*index] != NULL) {
+        strcpy(dir.d_name, entries[*index]);
+        ++(*index);
+        return &dir;
+    } else {
+        return NULL;
+    }
+}
+
+void ut_mcedata_tests::StoreNew(int dbhandle, orcm_db_data_type_t data_type,
+                                opal_list_t *input, opal_list_t *ret,
+                                orcm_db_callback_fn_t cbfunc, void *cbdata)
+{
+    cbfunc(dbhandle, ORCM_SUCCESS, input, ret, cbdata);
+}
+
+void ut_mcedata_tests::SendData(orcm_analytics_value_t *analytics_vals)
+{
 }
 
 
@@ -87,4 +144,221 @@ TEST_F(ut_mcedata_tests, mcedata_api_tests)
     // Cleanup
     orcm_sensor_base_runtime_metrics_destroy(object);
     mca_sensor_mcedata_component.runtime_metrics = NULL;
+}
+
+TEST_F(ut_mcedata_tests, mcedata_query_test)
+{
+    mca_base_module_t* module = NULL;
+    int priority = 0;
+
+    int rv = (*mca_sensor_mcedata_component.super.base_version.mca_query_component)(&module, &priority);
+    ASSERT_EQ(ORCM_SUCCESS, rv);
+    ASSERT_EQ(50, priority);
+    ASSERT_TRUE(NULL != module);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_open_close_test)
+{
+    int rv = (*mca_sensor_mcedata_component.super.base_version.mca_open_component)();
+    ASSERT_EQ(ORCM_SUCCESS, rv);
+    rv = (*mca_sensor_mcedata_component.super.base_version.mca_close_component)();
+    ASSERT_EQ(ORCM_SUCCESS, rv);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_register_test)
+{
+    int rv = (*mca_sensor_mcedata_component.super.base_version.mca_register_component_params)();
+    ASSERT_EQ(ORCM_SUCCESS, rv);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_init_start_stop_finalize_test)
+{
+    mcedata_mocking.opendir_callback = OpenDir;
+    mcedata_mocking.closedir_callback = CloseDir;
+    mcedata_mocking.readdir_callback = ReadDir;
+
+    mca_sensor_mcedata_component.use_progress_thread = false;
+
+    int rv = orcm_sensor_mcedata_module.init();
+    EXPECT_EQ(ORCM_SUCCESS, rv);
+
+    orcm_sensor_mcedata_module.start(2);
+    orcm_sensor_mcedata_module.stop(2);
+
+    orcm_sensor_mcedata_module.finalize();
+
+    mcedata_mocking.opendir_callback = NULL;
+    mcedata_mocking.closedir_callback = NULL;
+    mcedata_mocking.readdir_callback = NULL;
+}
+
+TEST_F(ut_mcedata_tests, mcedata_init_start_stop_finalize_test2)
+{
+    mca_sensor_mcedata_component.test = true;
+    mca_sensor_mcedata_component.use_progress_thread = true;
+    mca_sensor_mcedata_component.sample_rate = 1;
+
+    mcedata_mocking.opendir_callback = OpenDir;
+    mcedata_mocking.closedir_callback = CloseDir;
+    mcedata_mocking.readdir_callback = ReadDir;
+
+    int rv = orcm_sensor_mcedata_module.init();
+    EXPECT_EQ(ORCM_SUCCESS, rv);
+
+    orcm_sensor_mcedata_module.start(5);
+
+    sleep(2);
+
+    orcm_sensor_mcedata_module.stop(5);
+
+    orcm_sensor_mcedata_module.finalize();
+
+    mcedata_mocking.opendir_callback = NULL;
+    mcedata_mocking.closedir_callback = NULL;
+    mcedata_mocking.readdir_callback = NULL;
+    mca_sensor_mcedata_component.use_progress_thread = false;
+    mca_sensor_mcedata_component.test = false;
+}
+
+TEST_F(ut_mcedata_tests, mcedata_sample_log_test)
+{
+    mca_sensor_mcedata_component.test = true;
+    orcm_sensor_sampler_t sampler;
+    OBJ_CONSTRUCT(&sampler, orcm_sensor_sampler_t);
+
+    orcm_sensor_mcedata_module.sample(&sampler);
+
+    // Unpack whole buffer...
+    int n = 1;
+    opal_buffer_t* buffer = NULL;
+    int rv = opal_dss.unpack(&sampler.bucket, &buffer, &n, OPAL_BUFFER);
+    OBJ_DESTRUCT(&sampler);
+    EXPECT_EQ(ORCM_SUCCESS, rv);
+    EXPECT_FALSE(NULL == buffer);
+
+    // Unpack plugin name...
+    char* plugin = NULL;
+    n = 1;
+    rv = opal_dss.unpack(buffer, &plugin, &n, OPAL_STRING);
+    EXPECT_EQ(ORCM_SUCCESS, rv);
+    EXPECT_FALSE(NULL == plugin);
+    EXPECT_STREQ("mcedata", plugin);
+    if(NULL != plugin) {
+        free(plugin);
+        plugin = NULL;
+    }
+
+    orcm_analytics_API_module_send_data_fn_t saved = orcm_analytics.send_data;
+    orcm_analytics.send_data = SendData;
+    orcm_sensor_mcedata_module.log(buffer);
+    orcm_analytics.send_data = saved;
+
+    if(NULL != buffer) {
+        OBJ_RELEASE(buffer);
+        buffer = NULL;
+    }
+
+    mca_sensor_mcedata_component.test = false;
+}
+
+TEST_F(ut_mcedata_tests, mcedata_inventory_sample_log_test)
+{
+    opal_buffer_t buffer;
+    OBJ_CONSTRUCT(&buffer, opal_buffer_t);
+
+    orcm_sensor_mcedata_module.inventory_collect(&buffer);
+
+    // Unpack plugin name...
+    int n = 1;
+    char* plugin = NULL;
+    int rv = opal_dss.unpack(&buffer, &plugin, &n, OPAL_STRING);
+    EXPECT_EQ(ORCM_SUCCESS, rv);
+    EXPECT_FALSE(NULL == plugin);
+    EXPECT_STREQ("mcedata", plugin);
+    if(NULL != plugin) {
+        free(plugin);
+        plugin = NULL;
+    }
+    orcm_db_base_API_store_new_fn_t saved = orcm_db.store_new;
+    orcm_db.store_new = StoreNew;
+    orcm_sensor_mcedata_module.inventory_log((char*)"test_host", &buffer);
+    orcm_db.store_new = saved;
+
+    OBJ_DESTRUCT(&buffer);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_sample_rate_test)
+{
+    mca_sensor_mcedata_component.use_progress_thread = true;
+    mca_sensor_mcedata_component.sample_rate = 5;
+    int sample_rate = 0;
+    orcm_sensor_mcedata_module.get_sample_rate(&sample_rate);
+    EXPECT_EQ(5, sample_rate);
+    orcm_sensor_mcedata_module.set_sample_rate(7);
+    orcm_sensor_mcedata_module.get_sample_rate(&sample_rate);
+    EXPECT_EQ(7, sample_rate);
+    mca_sensor_mcedata_component.use_progress_thread = false;
+    orcm_sensor_mcedata_module.get_sample_rate(&sample_rate);
+    EXPECT_EQ(7, sample_rate);
+    orcm_sensor_mcedata_module.set_sample_rate(3);
+    orcm_sensor_mcedata_module.get_sample_rate(&sample_rate);
+    EXPECT_EQ(7, sample_rate);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_unknown_filter_test)
+{
+    unsigned long reg = 0;
+    opal_list_t* vals = OBJ_NEW(opal_list_t);
+    mcedata_unknown_filter(&reg, vals);
+    EXPECT_EQ(1, opal_list_get_size(vals));
+    ORCM_RELEASE(vals);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_bus_ic_filter_test)
+{
+    uint64_t mce_reg[MCE_REG_COUNT];
+    memset(mce_reg, 0, sizeof(mce_reg));
+    opal_list_t* vals = OBJ_NEW(opal_list_t);
+
+    mcedata_bus_ic_filter(mce_reg, vals);
+    EXPECT_EQ(6, opal_list_get_size(vals));
+
+    ORCM_RELEASE(vals);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_gen_cache_filter_test)
+{
+    uint64_t mce_reg[MCE_REG_COUNT];
+    memset(mce_reg, 0, sizeof(mce_reg));
+    opal_list_t* vals = OBJ_NEW(opal_list_t);
+
+    mcedata_gen_cache_filter(mce_reg, vals);
+    EXPECT_EQ(2, opal_list_get_size(vals));
+
+    ORCM_RELEASE(vals);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_tlb_filter_test)
+{
+    uint64_t mce_reg[MCE_REG_COUNT];
+    memset(mce_reg, 0, sizeof(mce_reg));
+    opal_list_t* vals = OBJ_NEW(opal_list_t);
+
+    mcedata_tlb_filter(mce_reg, vals);
+    EXPECT_EQ(1, opal_list_get_size(vals));
+
+    ORCM_RELEASE(vals);
+}
+
+TEST_F(ut_mcedata_tests, mcedata_cache_filter_test)
+{
+    uint64_t mce_reg[MCE_REG_COUNT];
+    memset(mce_reg, 0, sizeof(mce_reg));
+    mce_reg[2] = ((uint64_t)1<<63);
+    opal_list_t* vals = OBJ_NEW(opal_list_t);
+
+    mcedata_cache_filter(mce_reg, vals);
+    EXPECT_EQ(5, opal_list_get_size(vals));
+
+    ORCM_RELEASE(vals);
 }
--- orcm/test/mca/sensor/mcedata/mcedata_tests.h
+++ orcm/test/mca/sensor/mcedata/mcedata_tests.h
@@ -12,8 +12,14 @@
 
 #include "gtest/gtest.h"
 
+#include <sys/types.h>
+#include <dirent.h>
+
 #include <string>
 #include <vector>
+
+#include "orcm/mca/db/db.h"
+#include "orcm/mca/analytics/analytics.h"
 
 class ut_mcedata_tests: public testing::Test
 {
@@ -21,6 +27,15 @@ class ut_mcedata_tests: public testing::Test
         // gtest fixture required methods
         static void SetUpTestCase();
         static void TearDownTestCase();
+
+        static DIR* OpenDir(const char*);
+        static int CloseDir(DIR*);
+        static struct dirent* ReadDir(DIR*);
+
+        static void StoreNew(int dbhandle, orcm_db_data_type_t data_type,
+                             opal_list_t *input, opal_list_t *ret,
+                             orcm_db_callback_fn_t cbfunc, void *cbdata);
+        static void SendData(orcm_analytics_value_t *analytics_vals);
 }; // class
 
 #endif // MCEDATA_TESTS_H

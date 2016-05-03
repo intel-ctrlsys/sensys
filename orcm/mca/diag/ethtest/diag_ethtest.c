209a094e73da2e23387095de59d620e1ddd9e0a8
Ke Wang <ke1.wang@intel.com>
2016-05-03 16:54:23 -0700

    Fix Klockwork issue of diag ethtest
    Change-Id: I6c1ccaf294919c8f2dce08dbb517ddcd6c2433d7
    Signed-off-by: Ke Wang <ke1.wang@intel.com>
    (cherry picked from commit 9f19f2df9d04c3371e52c2fc5894fbc5c0523bd8)

--- orcm/mca/diag/ethtest/diag_ethtest.c
+++ orcm/mca/diag/ethtest/diag_ethtest.c
@@ -221,7 +221,6 @@ static void ethtest_run(int sd, short args, void *cbdata)
     char *diag_subtype;
     int  result_num = 0;
     orte_process_name_t *tgt;
-    size_t copy_size = 0;
 
     if (ORTE_PROC_IS_CM) {
         /* we send to our daemon */
@@ -250,13 +249,8 @@ static void ethtest_run(int sd, short args, void *cbdata)
         goto sendresults;
     }
 
-    copy_size = strlen(resource);
-    if (copy_size >= sizeof(ifr.ifr_name)) {
-        copy_size = 0;
-    } else {
-        strncpy(ifr.ifr_name, resource, copy_size);
-    }
-    ifr.ifr_name[copy_size] = '\0';
+    strncpy(ifr.ifr_name, resource, sizeof(ifr.ifr_name));
+    ifr.ifr_name[sizeof(ifr.ifr_name) - 1] = '\0';
 
      /* Get string set length */
     testinfo_offset = (int)offsetof(struct ethtool_drvinfo, testinfo_len);

58f102d3fa1666e123b03799eb733a52f57ab570
Paul Amonson <paul.d.amonson@intel.com>
2016-05-09 13:43:57 -0700

    Fixing more memory leaks. analytics_aggregate.c had the wrong date so I fixed it and whitespace.
    Change-Id: Ic1338f0d3a347317b66abdacb892969fcc872ccc
    Signed-off-by: Paul Amonson <paul.d.amonson@intel.com>

--- orcm/mca/analytics/aggregate/analytics_aggregate.c
+++ orcm/mca/analytics/aggregate/analytics_aggregate.c
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2014      Intel, Inc. All rights reserved.
+ * Copyright (c) 2015-2016 Intel, Inc. All rights reserved.
  * $COPYRIGHT$
- * 
+ *
  * Additional copyrights may follow
- * 
+ *
  * $HEADER$
  */
 

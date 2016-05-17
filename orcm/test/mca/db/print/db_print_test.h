9e70b309b996334a4008f1de3201a1341089cde2
Ke Wang <ke1.wang@intel.com>
2016-05-16 17:16:31 -0700

    Add TDD tests for the db print plugin
    Change-Id: I8e9462475d8ed4b6e75ce2e791ac2bac67fcc3c3
    Signed-off-by: Ke Wang <ke1.wang@intel.com>

---
+++ orcm/test/mca/db/print/db_print_test.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016      Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef GREI_ORCM_TEST_MCA_DB_PRINT_DB_PRINT_TEST_H_
+#define GREI_ORCM_TEST_MCA_DB_PRINT_DB_PRINT_TEST_H_
+
+extern "C" {
+    #include "orcm/mca/db/print/db_print.h"
+}
+
+#endif /* GREI_ORCM_TEST_MCA_DB_PRINT_DB_PRINT_TEST_H_ */

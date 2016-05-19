5fa13f00b0746a9f786edf8cf98879ea1db32622
Marcela Morales Quispe <marcela.morales.quispe@intel.com>
2016-05-18 23:54:56 -0500

    [RUNTIME] New UTs to improve branch coverage for orcm_globals.
    Change-Id: Ib7cb6fd2dac67281c114e6fa3f7c4d9a1d995311
    Signed-off-by: Marcela Morales Quispe <marcela.morales.quispe@intel.com>

---
+++ orcm/test/runtime/orcm_globals/orcmg_tests.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016  Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef ORCM_GLOBALS_TESTS_H
+#define ORCM_GLOBALS_TESTS_H
+
+#include "gtest/gtest.h"
+
+class ut_orcm_globals_tests: public testing::Test{
+};
+
+#endif // ORCM_GLOBALS_TESTS_H

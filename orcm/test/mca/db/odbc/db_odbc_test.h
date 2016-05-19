17bb0d9f9bbaa70e82a74ddca70a49d778402c47
Ke Wang <ke1.wang@intel.com>
2016-05-19 10:06:37 -0700

    Improve code coverage for db odbc plugin
    Change-Id: I6bb4aa2f586c3e1e52e7a3e451fa5f1007ff7e69
    Signed-off-by: Ke Wang <ke1.wang@intel.com>

---
+++ orcm/test/mca/db/odbc/db_odbc_test.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016      Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef GREI_ORCM_TEST_MCA_DB_ODBC_DB_ODBC_TEST_H_
+#define GREI_ORCM_TEST_MCA_DB_ODBC_DB_ODBC_TEST_H_
+
+extern "C" {
+    #include "orcm/mca/db/odbc/db_odbc.h"
+}
+
+#endif /* GREI_ORCM_TEST_MCA_DB_ODGC_DB_ODBC_TEST_H_ */

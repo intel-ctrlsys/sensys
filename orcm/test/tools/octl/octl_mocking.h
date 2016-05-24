0f8777a53c866c66c5430d4b2dfcae292a0c224f
Victor Escamilla <victor.escamilla.ochoa@intel.com>
2016-05-24 18:34:46 -0500

    Added more tests to increase coverage numbers for octl/chassis-id.c
    Change-Id: I7bcafc87eb359633bd8232d51ca4de221dcc85ef
    Signed-off-by: Victor Escamilla <victor.escamilla.ochoa@intel.com>

--- orcm/test/tools/octl/octl_mocking.h
+++ orcm/test/tools/octl/octl_mocking.h
@@ -25,9 +25,20 @@ extern "C" {
 
     extern int __real_opal_dss_pack(opal_buffer_t* buffer, const void* src, int32_t num_vals, opal_data_type_t type);
 
+    extern int __real_mca_base_framework_open(struct mca_base_framework_t *framework, mca_base_open_flag_t flags);
+
+    extern int __real_orcm_parser_base_select(void);
+
+    extern int __real_orcm_logical_group_parse_array_string(char* regex, char*** o_array_string);
+
 #ifdef __cplusplus
 }
 #endif // __cplusplus
+
+extern bool is_mca_base_framework_open_expected_to_succeed;
+extern bool is_orcm_parser_base_select_expected_to_succeed;
+extern bool is_get_bmc_info_expected_to_succeed;
+extern bool is_orcm_logical_group_parse_array_string_expected_to_succeed;
 
 typedef int (*orcm_cfgi_base_get_hostname_proc_callback_fn_t)(char* hostname, orte_process_name_t* proc);
 typedef int (*opal_dss_pack_fn_t)(opal_buffer_t* buffer, const void* src, int32_t num_vals, opal_data_type_t type);

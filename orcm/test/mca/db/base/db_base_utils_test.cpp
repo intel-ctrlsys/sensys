a5d92db5496c907356b5f170cc571bc8b4b7b270
Erich Cordoba <erich.cordoba.malibran@intel.com>
2016-05-25 14:39:49 -0500

    Add Ut for db/base in type conversions
    Change-Id: I5ed87df8235a6d0054c7e0a858a19ba4b4553f49
    Signed-off-by: Erich Cordoba <erich.cordoba.malibran@intel.com>

--- orcm/test/mca/db/base/db_base_utils_test.cpp
+++ orcm/test/mca/db/base/db_base_utils_test.cpp
@@ -15,9 +15,12 @@
 using namespace std;
 
 extern "C" {
+    #include "orcm/mca/db/base/base.h"
     bool time_stamp_str_to_tv(char *stamp, struct timeval* time_value);
     bool is_supported_opal_int_type(opal_data_type_t type);
     char *build_query_from_view_name_and_filters(const char* view_name, opal_list_t* filter);
+    char *get_opal_value_as_sql_string(opal_value_t *value);
+    int opal_value_to_orcm_db_item(const opal_value_t *kv, orcm_db_item_t *item);
 }
 
 void ut_db_base_utils_tests::SetUpTestCase()
@@ -172,4 +175,201 @@ TEST_F(ut_db_base_utils_tests, build_query_null_null)
     char *res = NULL;
     res = build_query_from_view_name_and_filters(NULL, NULL);
     ASSERT_TRUE(res == NULL);
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_null)
+{
+    char *res = NULL;
+    res = get_opal_value_as_sql_string(NULL);
+    ASSERT_TRUE(res == NULL);
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_int)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_INT;
+    test_value.data.integer = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_int8)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_INT8;
+    test_value.data.int8 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_int16)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_INT16;
+    test_value.data.int16 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_int32)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_INT32;
+    test_value.data.int32 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_int64)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_INT64;
+    test_value.data.int64 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_uint)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_UINT;
+    test_value.data.uint = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_uint8)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_UINT8;
+    test_value.data.uint8 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_uint16)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_UINT16;
+    test_value.data.uint16 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_uint32)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_UINT32;
+    test_value.data.uint32 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_uint64)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_UINT64;
+    test_value.data.uint64 = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    EXPECT_EQ(0, strcmp(res, "0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_float)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_FLOAT;
+    test_value.data.fval = 0.0;
+    res = get_opal_value_as_sql_string(&test_value);
+    ASSERT_TRUE(NULL != strstr(res, "0.0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_double)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_DOUBLE;
+    test_value.data.dval = 0.0;
+    res = get_opal_value_as_sql_string(&test_value);
+    ASSERT_TRUE(NULL != strstr(res, "0.0"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_timeval)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_TIMEVAL;
+    test_value.data.tv.tv_sec = 0;
+    test_value.data.tv.tv_usec = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    ASSERT_TRUE(NULL != strstr(res, "00:00:00.000"));
+}
+
+TEST_F(ut_db_base_utils_tests, get_opal_value_as_sql_time)
+{
+    char *res = NULL;
+    opal_value_t test_value;
+    test_value.type = OPAL_TIME;
+    test_value.data.tv.tv_sec = 0;
+    test_value.data.tv.tv_usec = 0;
+    res = get_opal_value_as_sql_string(&test_value);
+    ASSERT_TRUE(NULL != strstr(res, "00:00:00.000"));
+}
+
+TEST_F(ut_db_base_utils_tests, opal_value_to_orcm_db_uint8)
+{
+    int res;
+    opal_value_t test_value;
+    orcm_db_item_t res_value;
+    test_value.type = OPAL_UINT8;
+    test_value.data.uint8 = 0;
+    res = opal_value_to_orcm_db_item(&test_value, &res_value);
+    EXPECT_EQ(0, res);
+    EXPECT_EQ(test_value.data.uint8, res_value.value.value_int);
+}
+
+TEST_F(ut_db_base_utils_tests, opal_value_to_orcm_db_uint16)
+{
+    int res;
+    opal_value_t test_value;
+    orcm_db_item_t res_value;
+    test_value.type = OPAL_UINT16;
+    test_value.data.uint16 = 0;
+    res = opal_value_to_orcm_db_item(&test_value, &res_value);
+    EXPECT_EQ(0, res);
+    EXPECT_EQ(test_value.data.uint16, res_value.value.value_int);
+}
+
+TEST_F(ut_db_base_utils_tests, opal_value_to_orcm_db_uint32)
+{
+    int res;
+    opal_value_t test_value;
+    orcm_db_item_t res_value;
+    test_value.type = OPAL_UINT32;
+    test_value.data.uint32 = 0;
+    res = opal_value_to_orcm_db_item(&test_value, &res_value);
+    EXPECT_EQ(0, res);
+    EXPECT_EQ(test_value.data.uint32, res_value.value.value_int);
+}
+
+TEST_F(ut_db_base_utils_tests, opal_value_to_orcm_db_uint64)
+{
+    int res;
+    opal_value_t test_value;
+    orcm_db_item_t res_value;
+    test_value.type = OPAL_UINT64;
+    test_value.data.uint64 = 0;
+    res = opal_value_to_orcm_db_item(&test_value, &res_value);
+    EXPECT_EQ(0, res);
+    EXPECT_EQ(test_value.data.uint64, res_value.value.value_int);
 }

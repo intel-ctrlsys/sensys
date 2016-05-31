8fed1e96ebc898cb740685b551743d0b7d8821b8
dclaudio <david.claudio.gonzalez@intel.com>
2016-05-31 11:28:44 -0700

    Improve cc for resusage component
    Change-Id: I5d7bd24c47604c9d535b242ff160a81bfec5f243
    Signed-off-by: dclaudio <david.claudio.gonzalez@intel.com>

---
+++ orcm/test/mca/sensor/resusage/resusage_tests_mocking.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016  Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef RESUSAGE_TESTS_MOCKING_H
+#define RESUSAGE_TESTS_MOCKING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+    #include "orte/mca/state/state.h"
+    #include "orcm/mca/analytics/analytics_types.h"
+
+    extern orte_job_t* __real_orte_get_job_data_object(orte_jobid_t);
+    extern void __real_orcm_analytics_base_send_data(
+                orcm_analytics_value_t *data);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+typedef orte_job_t* (*orte_get_job_data_object_fn_t)(orte_jobid_t);
+typedef void (*orcm_analytics_base_send_data_fn_t)(orcm_analytics_value_t *data);
+
+
+class resusage_tests_mocking
+{
+    public:
+        // Construction
+        resusage_tests_mocking();
+
+        // Public Callbacks
+        orte_get_job_data_object_fn_t orte_get_job_data_object_callback;
+        orcm_analytics_base_send_data_fn_t orcm_analytics_base_send_data_callback;
+};
+
+extern resusage_tests_mocking resusage_mocking;
+
+#endif // RESUSAGE_TESTS_MOCKING_H

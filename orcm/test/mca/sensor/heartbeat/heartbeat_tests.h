a40dd30b8a71455ab3b14bbe08156fefc7c46576
Minia Oseguera <minia.oseguera@intel.com>
2016-06-02 14:24:03 -0500

    [SENSOR] Added tests for heartbeat sensor.
    Change-Id: I03f3a4f7e4a23821432e4e21f1c5edd48a28e237
    Signed-off-by: Minia Oseguera <minia.oseguera@intel.com>

---
+++ orcm/test/mca/sensor/heartbeat/heartbeat_tests.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016  Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#ifndef HEARTBEAT_TESTS_H
+#define HEARTBEAT_TESTS_H
+
+#include "gtest/gtest.h"
+#include "orcm/mca/sensor/heartbeat/sensor_heartbeat.h"
+
+extern "C" {
+    #include "opal/runtime/opal.h"
+    #include "orte/runtime/orte_globals.h"
+    #include "orte/mca/rml/rml.h"
+}
+
+#include <iostream>
+
+class ut_heartbeat_tests: public testing::Test {
+    protected:
+        // Mocking
+        static void RecvCancel(orte_process_name_t*, orte_rml_tag_t);
+        static void RecvBuffer(orte_process_name_t* peer, orte_rml_tag_t tag, bool persistent,
+                               orte_rml_buffer_callback_fn_t cbfunc,void* cbdata);
+        static int SendBuffer(orte_process_name_t* peer, struct opal_buffer_t* buffer, orte_rml_tag_t tag,
+                              orte_rml_buffer_callback_fn_t cbfunc, void* cbdata);
+        static int SendBufferReturnError(orte_process_name_t* peer, struct opal_buffer_t* buffer,
+                              orte_rml_tag_t tag, orte_rml_buffer_callback_fn_t cbfunc, void* cbdata);
+        static void OrteErrmgrBaseLog(int err, char* file, int lineno);
+        static char* OrteUtilPrintNameArgs(const orte_process_name_t *name);
+        static opal_event_base_t* OpalProgressThreadInit(const char* name);
+        static int OpalProgressThreadFinalize(const char* name);
+        static orte_job_t* OrteGetJobDataObject(orte_jobid_t job);
+        static void resetMockings();
+        static void setMockings();
+
+        static void SetUpTestCase();
+        virtual void SetUp();
+        static void TearDownTestCase();
+
+        static const char* proc_name_;
+        static int last_orte_error_;
+        static orte_job_t *orte_job;
+        static orte_rml_module_send_buffer_nb_fn_t saved_orte_send_buffer;
+        static orte_rml_module_recv_buffer_nb_fn_t saved_orte_recv_buffer;
+        static orte_rml_module_recv_cancel_fn_t saved_orte_recv_cancel;
+
+};
+
+#endif
---
+++ orcm/test/mca/sensor/heartbeat/heartbeat_tests_mocking.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016  Intel, Inc. All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ */
+
+#include "heartbeat_tests_mocking.h"
+
+#include <iostream>
+using namespace std;
+
+heartbeat_tests_mocking heartbeat_mocking;
+
+extern "C" { // Mocking must use correct "C" linkages
+    #include <stdarg.h>
+
+    void __wrap_orte_errmgr_base_log(int err, char* file, int lineno)
+    {
+        if(NULL == heartbeat_mocking.orte_errmgr_base_log_callback) {
+            __real_orte_errmgr_base_log(err, file, lineno);
+        } else {
+            heartbeat_mocking.orte_errmgr_base_log_callback(err, file, lineno);
+        }
+    }
+
+    char* __wrap_orte_util_print_name_args(const orte_process_name_t* name)
+    {
+        if(NULL == heartbeat_mocking.orte_util_print_name_args_callback) {
+            return __real_orte_util_print_name_args(name);
+        } else {
+            return heartbeat_mocking.orte_util_print_name_args_callback(name);
+        }
+    }
+
+    opal_event_base_t* __wrap_opal_progress_thread_init(const char* name)
+    {
+        if(NULL == heartbeat_mocking.opal_progress_thread_init_callback) {
+            return __real_opal_progress_thread_init(name);
+        } else {
+            return heartbeat_mocking.opal_progress_thread_init_callback(name);
+        }
+    }
+
+    int __wrap_opal_progress_thread_finalize(const char* name)
+    {
+        if(NULL == heartbeat_mocking.opal_progress_thread_finalize_callback) {
+            return __real_opal_progress_thread_finalize(name);
+        } else {
+            return heartbeat_mocking.opal_progress_thread_finalize_callback(name);
+        }
+    }
+
+    orte_job_t* __wrap_orte_get_job_data_object(orte_jobid_t job)
+    {
+        if(NULL == heartbeat_mocking.orte_get_job_data_object_callback) {
+            return __real_orte_get_job_data_object(job);
+        } else {
+            return heartbeat_mocking.orte_get_job_data_object_callback(job);
+        }
+    }
+
+} // extern "C"
+
+heartbeat_tests_mocking::heartbeat_tests_mocking() :
+    orte_errmgr_base_log_callback(NULL),
+    orte_util_print_name_args_callback(NULL),
+    opal_progress_thread_init_callback(NULL),
+    opal_progress_thread_finalize_callback(NULL)
+{
+}

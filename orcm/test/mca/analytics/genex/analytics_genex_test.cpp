23a4ad5eb060faaa6ce92048b85312790d3f0474
Hector Barajas <hector.a.barajas.villalobos@intel.com>
2016-06-08 04:33:48 -0400

    Improve CC for analytics genex plugin
    Change-Id: Ie9b4bf07030ebb9677b11eae76b7403ba9e8996e
    Signed-off-by: Hector Barajas <hector.a.barajas.villalobos@intel.com>

--- orcm/test/mca/analytics/genex/analytics_genex_test.cpp
+++ orcm/test/mca/analytics/genex/analytics_genex_test.cpp
@@ -9,61 +9,171 @@
 
 #include "gtest/gtest.h"
 #include "analytics_genex_test.h"
+#include "analytics_util.h"
+
+#define SAFE_FREE(x) if(NULL != x) free((void*)x); x=NULL
+#define SAFE_OBJ_FREE(x) if(NULL!=x) OBJ_RELEASE(x); x=NULL
+
+genex_workflow_value_t* create_workflow()
+{
+    return (genex_workflow_value_t*)malloc(sizeof(genex_workflow_value_t));
+}
+
+orcm_workflow_caddy_t* create_caddy()
+{
+    orcm_value_t* orcm_value;
+    orcm_workflow_caddy_t* caddy = OBJ_NEW(orcm_workflow_caddy_t);
+
+    opal_list_t* key = OBJ_NEW(opal_list_t);
+    opal_list_t* non_compute = OBJ_NEW(opal_list_t);
+    opal_list_t* compute = OBJ_NEW(opal_list_t);
+
+    caddy->analytics_value =
+        orcm_util_load_orcm_analytics_value(key, non_compute, compute);
+
+    orcm_value = analytics_util::load_orcm_value("severity", (void*)"crit", OPAL_STRING, "sev");
+    opal_list_append(caddy->analytics_value->compute_data, &orcm_value->value.super);
+
+    orcm_value = analytics_util::load_orcm_value("message", (void*)"hello", OPAL_STRING, "log");
+    opal_list_append(caddy->analytics_value->compute_data, &orcm_value->value.super);
+
+    return caddy;
+}
+
+void destroy_caddy(orcm_workflow_caddy_t* caddy)
+{
+    SAFE_OBJ_FREE(caddy->analytics_value->key);
+    SAFE_OBJ_FREE(caddy->analytics_value->compute_data);
+    SAFE_OBJ_FREE(caddy->analytics_value->non_compute_data);
+    SAFE_OBJ_FREE(caddy->analytics_value);
+    SAFE_OBJ_FREE(caddy);
+}
 
 TEST(analytics_genex, init_null_input)
 {
     int rc = orcm_analytics_genex_module.api.init(NULL);
+
     ASSERT_EQ(ORCM_ERROR, rc);
+}
+
+TEST(analytics_genex, finalize_input)
+{
+    orcm_analytics_base_module_t *imod =
+        (orcm_analytics_base_module_t*)malloc(sizeof(orcm_analytics_base_module_t));
+
+    if ( NULL != imod )
+    {
+        int rc = orcm_analytics_genex_module.api.init(imod);
+
+        ASSERT_EQ(ORCM_SUCCESS, rc);
+
+        orcm_analytics_genex_module.api.finalize(imod);
+    }
 }
 
 TEST(analytics_genex, analyze_null_input)
 {
     int rc = orcm_analytics_genex_module.api.analyze(0, 0, NULL);
+
     ASSERT_EQ(ORCM_ERROR, rc);
 }
 
 TEST(analytics_genex, monitor_null_input)
 {
     int rc = monitor_genex(NULL, NULL, NULL);
+
     ASSERT_EQ(ORCM_ERROR, rc);
 }
 
 TEST(analytics_genex, get_monitor_wrong_regex_input)
 {
     opal_list_t* event_list;
-    void *cbdata = malloc(1024);
+    orcm_workflow_caddy_t *caddy;
+    genex_workflow_value_t *wf_value;
 
-    genex_workflow_value_t *workflow_value =
-        (genex_workflow_value_t*)malloc(sizeof(genex_workflow_value_t));
+    caddy = analytics_util::create_caddy(NULL, NULL, NULL, NULL, NULL);
 
-    workflow_value->msg_regex = strdup("(()");
-    int rc = monitor_genex(workflow_value,cbdata,event_list);
+    wf_value = create_workflow();
+    wf_value->msg_regex = strdup("(()");
+
+    int rc = monitor_genex(wf_value, caddy, event_list);
+
+    SAFE_OBJ_FREE(caddy);
+    SAFE_FREE(wf_value);
+
+    ASSERT_EQ(ORCM_ERROR, rc);
+}
+
+TEST(analytics_genex, get_monitor_wrong_notifier)
+{
+    opal_list_t* event_list;
+
+    orcm_workflow_caddy_t* caddy = create_caddy();
+
+    genex_workflow_value_t *wf_value = create_workflow();
+
+    wf_value->msg_regex = strdup("hello");
+    wf_value->severity = strdup("crit");
+
+    int rc = monitor_genex(wf_value, caddy, event_list);
+
+    SAFE_FREE(wf_value);
+
+    destroy_caddy(caddy);
+
     ASSERT_EQ(ORCM_ERROR, rc);
 
-    free(workflow_value);
-    free(cbdata);
 }
 
 TEST(analytics_genex, get_genex_policy_workflow_null_input)
 {
     int rc = get_genex_policy(NULL, NULL);
+
     ASSERT_EQ(ORCM_ERROR, rc);
 }
 
 TEST(analytics_genex, get_genex_policy_wfvalue_null_input)
 {
-    void *workflow_value = malloc(sizeof(genex_workflow_value_t));
-    int rc = get_genex_policy(NULL, (genex_workflow_value_t*)workflow_value);
+    genex_workflow_value_t *wf_value = create_workflow();
+
+    int rc = get_genex_policy(NULL, wf_value);
+
+    SAFE_FREE(wf_value);
+
     ASSERT_EQ(ORCM_ERROR, rc);
-    free(workflow_value);
 }
 
 TEST(analytics_genex, get_genex_policy_wrong_cbdata_input)
 {
     void *cbdata = malloc(1024);
-    void *workflow_value = malloc(sizeof(genex_workflow_value_t));
-    int rc = get_genex_policy(cbdata, (genex_workflow_value_t*)workflow_value);
+
+    genex_workflow_value_t *wf_value = create_workflow();
+
+    int rc = get_genex_policy(cbdata, wf_value);
+
+    SAFE_FREE(wf_value);
+    SAFE_FREE(cbdata);
+
     ASSERT_EQ(ORCM_ERROR, rc);
-    free(workflow_value);
-    free(cbdata);
+}
+
+TEST(analytics_genex, analize_monitor_genex_failure)
+{
+    orcm_workflow_caddy_t* caddy = create_caddy();
+
+    orcm_analytics_base_module_t *mod =
+        (orcm_analytics_base_module_t*)malloc(sizeof(orcm_analytics_base_module_t));
+
+    caddy->imod = (orcm_analytics_base_module_t*)mod;
+
+    caddy->wf = OBJ_NEW(orcm_workflow_t);
+    caddy->wf_step = OBJ_NEW(orcm_workflow_step_t);
+
+    int rc = orcm_analytics_genex_module.api.analyze(0, 0, caddy);
+
+    SAFE_FREE(mod);
+    SAFE_OBJ_FREE(caddy->wf);
+    SAFE_OBJ_FREE(caddy->wf_step);
+
+    ASSERT_EQ(ORCM_ERROR, rc);
 }

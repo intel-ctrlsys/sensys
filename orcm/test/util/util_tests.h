dd6108ebd3fc35190494986df10d61c8333f608a
Mariana Gonzalez Velarde <mariana.gonzalez.velarde@intel.com>
2016-06-15 21:51:26 -0400

    Add test content for cli.c
    Change-Id: I35dc3b491041da21b06bb0a960696f7b0b99f4e4
    Signed-off-by: Mariana Gonzalez Velarde <mariana.gonzalez.velarde@intel.com>
    (cherry picked from commit 3cac3e4e8ae80ec302459be67114566a91e0ad89)

--- orcm/test/util/util_tests.h
+++ orcm/test/util/util_tests.h
@@ -11,16 +11,7 @@
 #define UTIL_TESTS_H
 
 #include "gtest/gtest.h"
-#include "orcm/util/string_utils.h"
 #include "util_tests_mocking.h"
-
-extern "C" {
-    #include "orcm/util/attr.h"
-    #include "orcm/util/cli.h"
-    #include "orcm/util/logical_group.h"
-    #include "orcm/util/utils.h"
-    #include "opal/runtime/opal.h"
-}
 
 class ut_util_tests: public testing::Test {
     protected:

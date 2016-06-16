e66a79807251f67b858ea0478e6b515760b6faee
moralesq <marcela.morales.quispe@intel.com>
2016-06-16 17:59:33 -0500

    [SIGAR] Added new UTs in order to increase code coverage rates.
    Change-Id: Ib9e76e458c9ba5a098c4e705e500681abc5d60ec
    Signed-off-by: moralesq <marcela.morales.quispe@intel.com>

--- orcm/test/mca/sensor/sigar/sigar_mocked_functions.h
+++ orcm/test/mca/sensor/sigar/sigar_mocked_functions.h
@@ -14,34 +14,47 @@
 #include <iostream>
 
 #include "orcm/mca/sensor/base/sensor_runtime_metrics.h"
+#include "orcm/util/utils.h"
 
 #ifdef __cplusplus
 extern "C"{
 #endif
     extern bool __real_orcm_sensor_base_runtime_metrics_do_collect(void*, const char*);
+    extern orcm_value_t* __real_orcm_util_load_orcm_value(char*, void*, opal_data_type_t, char*);
 #ifdef __cplusplus
 }
 #endif
 
-const int collect_mem_size=4;
-const int collect_swap_size=4;
-const int collect_cpu_size=3;
-const int collect_load_size=3;
-const int collect_disk_size=11;
-const int collect_network_size=10;
-const int collect_gproc_size=7;
-const int collect_proc_size=14;
+const int sigar_mem_size = 4;
+const int sigar_swap_size = 4;
+const int sigar_cpu_size = 3;
+const int sigar_load_size = 3;
+const int sigar_disk_size = 11;
+const int sigar_network_size = 10;
+const int sigar_gproc_size = 7;
+const int sigar_proc_size = 14;
 
 extern bool collect_sample_flag;
 extern bool collect_sys_success_flag;
-extern bool collect_mem_success_flag[collect_mem_size];
-extern bool collect_swap_success_flag[collect_swap_size];
-extern bool collect_cpu_success_flag[collect_cpu_size];
-extern bool collect_load_success_flag[collect_load_size];
-extern bool collect_disk_success_flag[collect_disk_size];
-extern bool collect_network_success_flag[collect_network_size];
-extern bool collect_gproc_success_flag[collect_gproc_size];
-extern bool collect_proc_success_flag[collect_proc_size];
+extern bool collect_mem_success_flag[sigar_mem_size];
+extern bool collect_swap_success_flag[sigar_swap_size];
+extern bool collect_cpu_success_flag[sigar_cpu_size];
+extern bool collect_load_success_flag[sigar_load_size];
+extern bool collect_disk_success_flag[sigar_disk_size];
+extern bool collect_network_success_flag[sigar_network_size];
+extern bool collect_gproc_success_flag[sigar_gproc_size];
+extern bool collect_proc_success_flag[sigar_proc_size];
+
+extern bool uvalue_sample_flag;
+extern bool uvalue_sys_success_flag;
+extern bool uvalue_mem_success_flag[sigar_mem_size];
+extern bool uvalue_swap_success_flag[sigar_swap_size];
+extern bool uvalue_cpu_success_flag[sigar_cpu_size];
+extern bool uvalue_load_success_flag[sigar_load_size];
+extern bool uvalue_disk_success_flag[sigar_disk_size];
+extern bool uvalue_network_success_flag[sigar_network_size];
+extern bool uvalue_gproc_success_flag[sigar_gproc_size];
+extern bool uvalue_proc_success_flag[sigar_proc_size];
 
 bool eval_collect_mem(const char* sensor_spec);
 bool eval_collect_swap(const char* sensor_spec);
@@ -52,6 +65,17 @@ bool eval_collect_network(const char* sensor_spec);
 bool eval_collect_system(const char* sensor_spec);
 bool eval_collect_gproc(const char* sensor_spec);
 bool eval_collect_proc(const char* sensor_spec);
-bool eval_sensor_spec(const char* sensor_spec);
 
+bool eval_uvalue_mem(char* key);
+bool eval_uvalue_swap(char* key);
+bool eval_uvalue_cpu(char* key);
+bool eval_uvalue_load(char* key);
+bool eval_uvalue_disk(char* key);
+bool eval_uvalue_network(char* key);
+bool eval_uvalue_system(char* key);
+bool eval_uvalue_gproc(char* key);
+bool eval_uvalue_proc(char* key);
+
+bool eval_collect_spec(const char* sensor_spec);
+bool eval_uvalue_key(char* key);
 #endif // SIGAR_MOCKED_FUNCTIONS_H

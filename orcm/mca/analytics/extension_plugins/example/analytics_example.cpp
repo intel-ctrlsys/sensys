c455203f0fb22ba8a11ba07916688bbbea26fe0b
Shweta S Kulkarni <shweta.s.kulkarni@intel.com>
2016-08-23 08:37:01 -0700

    Edit example plugin to compute sample average and fire events to enable BAT tests
    Change-Id: I05d2eba2febea2b4d1babecddac51eaf7b29ebfe
    Signed-off-by: Shweta S Kulkarni <shweta.s.kulkarni@intel.com>

--- orcm/mca/analytics/extension_plugins/example/analytics_example.cpp
+++ orcm/mca/analytics/extension_plugins/example/analytics_example.cpp
@@ -30,6 +30,28 @@ Example::~Example()
 int Example::analyze(DataSet& data_set)
 {
     std::cout << "This is an example plugin!" << std::endl;
+
+    //Example to compute sample average
+    dataContainer::iterator i = data_set.compute.begin();
+    double avg, sum = 0.0;
+    int n = 0;
+    while(i != data_set.compute.end()) {
+        sum += data_set.compute.getValue<double>(i);
+        n++;
+        i++;
+    }
+    avg = sum / n;
+    data_set.results.put<double>("Example_key", avg, "C");
+
+    //Example to create an event for a sample
+    Event ev = {
+    "crit",
+    "syslog",
+    "Test_example",
+    avg,
+    "C",
+    };
+    data_set.events.push_back(ev);
     return 0;
 }
 

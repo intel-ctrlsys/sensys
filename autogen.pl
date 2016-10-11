7f1786f44cd1ff2ff8ff2ab151c19f65b6421678
Erich Cordoba <erich.cordoba.malibran@intel.com>
2016-10-11 13:33:37 -0500

    Change project name and version.
    Change-Id: If360b3e79d91c35e1a9522c63e8c4eae548cd8cb
    (cherry picked from commit b998d8d003bd13d34010328da39699d73e85d953)

--- autogen.pl
+++ autogen.pl
@@ -832,7 +832,7 @@ if (!$ok || $help_arg) {
 #---------------------------------------------------------------------------
 
 # Check for project existence
-my $project_name_long = "Open Resilient Cluster Manager";
+my $project_name_long = "Sensor System for High Performance Computing";
 my $project_name_short = "openrcm";
 
 #---------------------------------------------------------------------------

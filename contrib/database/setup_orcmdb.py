5687a1e4b3a020bcde972804b91e2dbfedfd31e7
David Romero Antequera <david.romero.antequera@intel.com>
2017-02-22 17:35:26 -0600

    Appropriate error message on the setup_orcmdb.py script
    Change-Id: I6a5db06f5d2e7618332f843b826bf381b180dfff
    Signed-off-by: David Romero Antequera <david.romero.antequera@intel.com>

--- contrib/database/setup_orcmdb.py
+++ contrib/database/setup_orcmdb.py
@@ -11,23 +11,25 @@ import models
 
 def main():
     """Command line interface for setup database"""
-    parser = ArgumentParser(description="Setup ORCM database")
-    parser.add_argument("--alembic-ini", dest='alembic_ini',
-                        metavar='/path/to/alembic/config/file',
-                        type=str, default="schema_migration.ini",
-                        help="A path to the Alembic config file")
-    args = parser.parse_args()
-
-    save_cwd = os.getcwd()
-    ini_dir, ini_file = os.path.split(args.alembic_ini)
     try:
+        parser = ArgumentParser(description="Setup ORCM database")
+        parser.add_argument("--alembic-ini", dest='alembic_ini',
+                            metavar='/path/to/alembic/config/file',
+                            type=str, default="schema_migration.ini",
+                            help="A path to the Alembic config file")
+        args = parser.parse_args()
+
+        save_cwd = os.getcwd()
+        ini_dir, ini_file = os.path.split(args.alembic_ini)
         os.chdir(os.path.join(save_cwd, ini_dir))
         models.setup(alembic_ini=ini_file)
     except OperationalError as e:
-        print("\nException: %s\n" % e)
+        print("\nError: %s\n" % e)
         print("Make sure the database exists and the current user has "
               "proper permissions to create tables and issue DDL & DML "
               "statements.")
+    except RuntimeError as e:
+        print("\nError: %s\n" % e)
     finally:
         os.chdir(save_cwd)
 
